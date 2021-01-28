f84ba51b73d6535c42bf01b578497f146871f2c7
Ilchenko <ilchenkowd@gmail.com>
2021-01-28 12:52:45 +0500

    Добавлены скрипты на все страницы

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ let loginForm = document.querySelector(".user-list__login-form");
 let loginFormTablet = document.querySelector(".login-popup");
 let loginFormClose = document.querySelector(".login-popup__close");
 
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav__toggle");
+
+// login-popup
 login.addEventListener("click", function (e) {
     e.preventDefault();
     loginForm.classList.toggle("user-list__login-form--opened");
@@ -12,4 +16,18 @@ login.addEventListener("click", function (e) {
 loginFormClose.addEventListener("click", function (e) {
     e.preventDefault();
     loginFormTablet.classList.toggle("login-popup--opened");
+});
+
+
+// nav-menu
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function () {
+    if (navMain.classList.contains("main-nav--closed")) {
+        navMain.classList.remove("main-nav--closed");
+        navMain.classList.add("main-nav--opened");
+    } else {
+        navMain.classList.remove("main-nav--opened");
+        navMain.classList.add("main-nav--closed");
+    }
 });
